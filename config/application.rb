e3e17bfea5a4ef461554645b82ce8ca75ae0dfb1
Osho Giri <oshopgiri@hotmail.com>
2022-11-03 21:52:09 +0000

    always passing default scope to bundler

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ class Application
   ENVIRONMENTS = %i[development test production].freeze
 
   def self.groups
-    @@groups ||= [:default, ENV['APP_ENV']&.split(',')&.map(&:to_sym)].flatten.compact.keep_if { |env| ENVIRONMENTS.include? env }
+    @groups ||= [
+      (ENV['WORKFLOW_ENVIRONMENT'] || ENV['APP_ENV'])&.split(',')&.map(&:to_sym)
+    ].flatten.compact.keep_if { |env| ENVIRONMENTS.include? env }
   end
 end
