fe378e07f345333687fcb09bc33b78f1b9a64ad7
Osho Giri <oshopgiri@hotmail.com>
2022-12-02 12:58:58 +0000

    updating slack ruby client

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,3 +1,8 @@
 Slack.configure do |config|
   config.token = ENV['SLACK_BOT_TOKEN']
 end
+
+Slack::Web::Client.configure do |config|
+  config.ca_file = OpenSSL::X509::DEFAULT_CERT_FILE
+  config.ca_path = OpenSSL::X509::DEFAULT_CERT_DIR
+end
