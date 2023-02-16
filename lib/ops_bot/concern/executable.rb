e9a16e10d2b32a8417a88d91c1756f91b5da3bad
Osho Giri <oshopgiri@hotmail.com>
2023-02-16 11:40:47 +0530

    adding debug logs

--- lib/ops_bot/concern/executable.rb
+++ lib/ops_bot/concern/executable.rb
@@ -4,6 +4,8 @@ module OpsBot::Concern::Executable
   class_methods do
     def execute
       result = perform
+      puts 'result from perform'
+      puts result
       result ? 0 : 1
     rescue
       1
