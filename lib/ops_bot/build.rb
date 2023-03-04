e8b2b12c6bb45a2a6159ffaae9de8c6e7de8153b
Osho Giri <oshopgiri@hotmail.com>
2023-03-04 13:02:42 +0000

    reporting unsupported build type

--- lib/ops_bot/build.rb
+++ lib/ops_bot/build.rb
@@ -1,6 +1,15 @@
 module OpsBot::Build
+  class UnsupportedTypeError < StandardError
+    def initialize(type:)
+      msg = "Build type `#{type}` not supported (yet!)"
+      super(msg)
+    end
+  end
+
   def self.new
     type = OpsBot::Context.env.build.type
     "#{self.name}::#{type.camelize}".constantize.new
+  rescue NameError => e
+    raise UnsupportedTypeError.new(type: type)
   end
 end
