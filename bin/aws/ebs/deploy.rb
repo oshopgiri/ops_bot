f44d915ad58a905fe5155f85f2b5a3816a190fb0
Osho Giri <oshopgiri@hotmail.com>
2023-03-04 22:28:38 +0000

    refractoring bin

--- bin/aws/ebs/deploy.rb
+++ bin/aws/ebs/deploy.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'pathname'
 boot_script = File.join(
-  Pathname.new('/').relative_path_from(Pathname.new("/#{File.dirname(__FILE__)}")),
+  Pathname.new('/').relative_path_from(Pathname.new("/#{File.dirname(__FILE__)}")).to_s,
   'config/boot.rb'
 )
 require_relative boot_script
