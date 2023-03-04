df329b813cb42ee03c54c1050827250594eae73e
Osho Giri <oshopgiri@hotmail.com>
2023-03-04 13:03:01 +0000

    checking github auth before setting repo secrets

--- lib/ops_bot/integration/github.rb
+++ lib/ops_bot/integration/github.rb
@@ -2,10 +2,12 @@ class OpsBot::Integration::GitHub
   attr_reader :repository
 
   def initialize(repository:)
-    @repository = repository
+    is_authenticated = system('gh auth status')
+    @repository = is_authenticated ? repository : nil
   end
 
   def set_action_secret(name:, value:)
+    return unless @repository
     system("gh secret set #{name} -a actions -b #{value} -R #{@repository}")
   end
 end
