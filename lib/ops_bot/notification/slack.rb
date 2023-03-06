916e889383af403410650835d5aab85b2abf57eb
Osho Giri <oshopgiri@hotmail.com>
2023-03-06 20:12:53 +0000

    fixing slack InvalidBlocksFormat error when notifying for failed key rotation

--- lib/ops_bot/notification/slack.rb
+++ lib/ops_bot/notification/slack.rb
@@ -3,7 +3,7 @@ class OpsBot::Notification::Slack < OpsBot::Notification::Base
     @channel_ids = begin
       client.auth_test
       OpsBot::Context.env.slack.channel_ids
-    rescue
+    rescue Slack::Web::Api::Errors::NotAuthed
       nil
     end
   end
@@ -13,17 +13,17 @@ class OpsBot::Notification::Slack < OpsBot::Notification::Base
 
     client.chat_postMessage(
       channel: @channel_ids,
-      blocks: self.render(
+      blocks: self.class.render(
         view_file: template,
         instance: self,
-        payload: payload
+        payload:
       )
     )
   end
 
   private
 
-    def client
-      @client ||= ::Slack::Web::Client.new
-    end
+  def client
+    @client ||= ::Slack::Web::Client.new
+  end
 end
