4da27d288ddfa7fd1e729df96626cf8823bc18e4
Osho Giri <oshopgiri@hotmail.com>
2023-03-06 09:10:14 +0000

    rubocop cleanup

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/GlobalVars
-
 $logger = Logger.new($stdout)
 original_formatter = Logger::Formatter.new
 $logger.formatter = proc { |severity, datetime, progname, msg|
   "\n#{original_formatter.call(severity, datetime, progname, msg)}"
 }
-
-# rubocop:enable Style/GlobalVars
