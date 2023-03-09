01e8075c31f49edefed722a76c9bde6b96a9b324
Osho Giri <oshopgiri@hotmail.com>
2023-03-09 20:56:09 +0000

    adding retry to deploy

--- lib/ops_bot/integration/aws/s3.rb
+++ lib/ops_bot/integration/aws/s3.rb
@@ -12,6 +12,16 @@ class OpsBot::Integration::AWS::S3
       .batch_delete!
   end
 
+  def delete_objects_older_than(time:)
+    return if time.to_i <= 0
+
+    resource
+      .bucket(@bucket)
+      .objects
+      .filter { |obj| obj.last_modified < time.ago }
+      .each(&:delete)
+  end
+
   def file_exists?(key:)
     client.head_object(bucket: @bucket, key:)
     true
