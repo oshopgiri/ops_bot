71dc52a769dc87276d8384c4b91f8eca87af9393
Osho Giri <oshopgiri@hotmail.com>
2023-03-13 18:00:27 +0000

    refractoring OpsBot::Integration::AWS::EC2::SecurityGroup

--- lib/ops_bot/integration/aws/ec2/security_group.rb
+++ lib/ops_bot/integration/aws/ec2/security_group.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
 class OpsBot::Integration::AWS::EC2::SecurityGroup
-  def initialize
-    @ip_address = OpsBot::Context.env.aws.ec2.security_group.rule.ip_address
-    @port = OpsBot::Context.env.aws.ec2.security_group.rule.port
-    @protocol = OpsBot::Context.env.aws.ec2.security_group.rule.protocol
-
-    @security_group_id = OpsBot::Context.secrets.aws.ec2.security_group.id
-
-    @description = OpsBot::Context.utils.aws.ec2.security_group.rule.description
+  def initialize(security_group_id:)
+    @security_group_id = security_group_id
   end
 
-  def revoke_old_ips
+  def revoke_old_ips(description_matcher:)
     existing_security_group_rule_ids = client
                                          .describe_security_group_rules(
                                            {
@@ -24,7 +18,7 @@ class OpsBot::Integration::AWS::EC2::SecurityGroup
                                            }
                                          )
                                          .security_group_rules
-                                         .select { |rule| rule.description.eql? @description }
+                                         .select { |rule| rule.description.include? description_matcher }
                                          .map(&:security_group_rule_id)
 
     return unless existing_security_group_rule_ids.present?
@@ -38,22 +32,22 @@ class OpsBot::Integration::AWS::EC2::SecurityGroup
       )
   end
 
-  def whitelist_ip
+  def whitelist_ip(description:, ip_address:, port:, protocol:)
     client
       .authorize_security_group_ingress(
         {
           group_id: @security_group_id,
           ip_permissions: [
             {
-              ip_protocol: @protocol,
+              ip_protocol: protocol,
               ip_ranges: [
                 {
-                  cidr_ip: "#{@ip_address}/32",
-                  description: @description
+                  cidr_ip: "#{ip_address}/32",
+                  description:
                 }
               ],
-              from_port: @port,
-              to_port: @port
+              from_port: port,
+              to_port: port
             }
           ]
         }
--- lib/ops_bot/job/aws/ec2/security_group/whitelist_ip.rb
+++ lib/ops_bot/job/aws/ec2/security_group/whitelist_ip.rb
@@ -2,9 +2,19 @@
 
 class OpsBot::Job::AWS::EC2::SecurityGroup::WhitelistIP < OpsBot::Job::Base
   def self.perform
-    security_group_client = OpsBot::Integration::AWS::EC2::SecurityGroup.new
-    security_group_client.revoke_old_ips
-    security_group_client.whitelist_ip
+    security_group_client = OpsBot::Integration::AWS::EC2::SecurityGroup.new(
+      security_group_id: OpsBot::Context.secrets.aws.ec2.security_group.id
+    )
+
+    security_group_client.revoke_old_ips(
+      description_matcher: OpsBot::Context.utils.aws.ec2.security_group.rule.description
+    )
+    security_group_client.whitelist_ip(
+      description: OpsBot::Context.utils.aws.ec2.security_group.rule.description,
+      ip_address: OpsBot::Context.env.aws.ec2.security_group.rule.ip_address,
+      port: OpsBot::Context.env.aws.ec2.security_group.rule.port,
+      protocol: OpsBot::Context.env.aws.ec2.security_group.rule.protocol
+    )
 
     true
   end
