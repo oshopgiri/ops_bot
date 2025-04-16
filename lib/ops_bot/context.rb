4294dacb67fe6c5b6d7275532bdb3f038b12efd0
Osho Giri <oshopgiri@hotmail.com>
2025-04-16 16:00:16 +0100

    Fixing SSL issue for Slack client (#14)
    * bumping all gems
    
    * updating bundler
    
    * reverting pathname to 0.2.0
    
    * fixing OpenStruct reference
    
    * fixing OpenStruct reference
    
    * disable ca check on slack client

--- lib/ops_bot/context.rb
+++ lib/ops_bot/context.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'ostruct'
 
 class OpsBot::Context
   include OpsBot::Concern::Renderable
@@ -18,21 +19,21 @@ class OpsBot::Context
   def self.build_env
     @env = JSON.parse(
       render(view_file: 'env.json.erb'),
-      object_class: OpenStruct
+      object_class: ::OpenStruct
     )
   end
 
   def self.build_secrets
     @secrets = JSON.parse(
       render(view_file: 'secrets.json.erb'),
-      object_class: OpenStruct
+      object_class: ::OpenStruct
     )
   end
 
   def self.build_utils
     @utils = JSON.parse(
       render(view_file: 'utils.json.erb'),
-      object_class: OpenStruct
+      object_class: ::OpenStruct
     )
   end
 end
